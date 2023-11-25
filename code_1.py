916ef3b237f5e94ad02e92f4f95766e63f2d9df8
LikaShu <142459648+LikaShu@users.noreply.github.com>
2023-11-25 20:42:04 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,57 @@
+function generateRandomData() {
+
+    });
+main();
+    const data = [];
+function main() {
+}
+    const data = generateRandomData();
+    return data;
+}
+
+    for (let i = 0; i < 10; i++) {
+        data.push(Math.floor(Math.random() * 100) + 1);
+        console.log("Random Number: " + item);
+
+    data.forEach(item => {
+    }        }
+            System.out.println("Random Number: " + item);
+        }
+    public static void main(String[] args) {
+
+        int[] data = new int[10];
+}
+        for (int i = 0; i < 10; i++) {
+    }
+public class RandomDataGenerator {
+
+        for (int item : data) {
+            data[i] = (int) (Math.random() * 100) + 1;}
+    const data = [];
+    data.forEach(item => {
+    for (let i = 0; i < 10; i++) {
+    return data;
+    }
+function generateRandomData() {
+
+
+        console.log("Random Number: " + item);
+        data.push(Math.floor(Math.random() * 100) + 1);
+main();
+    });
+}
+function main() {
+    const data = generateRandomData();
+        print(f"Random Number: {item}")
+
+
+def generate_random_data():
+    for item in data:
+def main():
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+import random
+
+    data = generate_random_data()
+    main()
